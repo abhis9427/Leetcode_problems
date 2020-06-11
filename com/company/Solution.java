3dc6fc25b2c8875fdbfacd249332874dd9b33237
abhis9427 <abhis.9427@gmail.com>
2020-06-12 02:59:53 +0530

    Accepted solution

--- com/company/Solution.java
+++ com/company/Solution.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 class Solution {
     ArrayList<ArrayList<Integer>> graph;
-    Map<Integer, Boolean> m_visit = new HashMap<>();
+    Map<Integer, Boolean> m_visit ;
     int[] color;
     int flag = 0;
 
@@ -27,19 +27,15 @@ class Solution {
         }
     }
 
-
     void addDirectedEdge(int v, int u) {
         graph.get(v).add(u);
     }
-
-    /*boolean visited(int source) {
-        return m_visit.containsKey(source);
-    }*/
 
     boolean colorTraversing(int source) {
         int clrOfSrc = color[source];
         //vist only white vertices
         if (clrOfSrc == 0) {
+            m_visit.put(source,true);
             //Making it grey
             color[source] = 1;
             System.out.println(source);
@@ -61,47 +57,17 @@ class Solution {
         return true;
     }
 
-    /*boolean traversal(int source) {
-        if (!visited(source)) {
-            m_visit.put(source, true);
-            List<Integer> neighbours = graph.get(source);
-            for (Integer neighbour : neighbours) {
-                boolean notCycle = traversal(neighbour);
-                if (!notCycle) {
-                    return false;
-                }
-            }
-        } else {
-            //System.out.println("is cycle");
-            return false;
-        }
-
-        return true;
-    }*/
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        m_visit=new HashMap<>();
+        for(int i=0;i<numCourses;i++){
+            m_visit.put(i,false);
+            System.out.println("false 1 ckpt :  "+i);
+        }
         if (prerequisites.length == 0) {
             return true;
         }
         boolean ans = true;
-        int[] getSrc=new int[numCourses];
-        int src=prerequisites[0][0];
-        if(prerequisites.length>3){
-            int value = 0;
-            for (int i = 0; i < numCourses; i++) {
-
-                getSrc[i] = 0;
-            }
-            for (int i = 0; i < prerequisites.length; i++) {
-                getSrc[prerequisites[i][0]] += 1;
-            }
-            for (int i = 0; i < numCourses; i++) {
-                if (value <= getSrc[i]) {
-                    src = i;
-                }
-            }
-        }
-
 
         colorCodingNode(numCourses);
         constructGraph(numCourses);
@@ -111,8 +77,14 @@ class Solution {
                 addDirectedEdge(prerequisites[i][0], prerequisites[i][j]);
             }
         }
-        System.out.println("sorce : "+src);
-        colorTraversing(src);
+        System.out.println("sorce : " + prerequisites[0][0]);
+        colorTraversing(prerequisites[0][0]);
+        for(int i=0;i<numCourses;i++){
+            if(m_visit.get(i)==null|| !m_visit.get(i)){
+                System.out.println("false"+i);
+                colorTraversing(i);
+            }
+        }
         if (flag == 1) {
             ans = false;
         }
